9eb748a56d44bdd8e45536d15436d95c06e9eb56
anusha <anusha.j@thundersoft.com>
2022-11-18 18:50:27 +0530

    18-11-2022

--- QC/string_reverse.c
+++ QC/string_reverse.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 #include<string.h>
-void reverse(char s[])
+void reverse(char s[],int index, int size)
 {
 	char temp;
-        int i,len;
-        len=strlen(s)-1;
-        for(i=0;i<strlen(s)/2;i++)
-        {
-                temp=s[i];
-                s[i]=s[len];
-                s[len--]=temp;
-        }
-        printf("Reverse string is : %s\n",s);
+	temp = s[index];
+	s[index] = s[size - index];
+	s[size - index] = temp;
+	if(index == size/2)
+	{
+		return;
+	}
+	reverse(s, index+1, size);
 }
 
 void main()
 {
 	char s[20];
+	int size;
 	printf("Enter string:");
-	scanf("%s",s);
-	reverse(s);
+	gets(s);
+	size=strlen(s);
+	reverse(s, 0, size-1);
+	printf("Reverse string is : %s\n",s);
 	
 }
 
